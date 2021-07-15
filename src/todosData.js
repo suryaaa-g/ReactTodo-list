516e307216c4860b0e29d20d82acf3c307e08909
Suryateja <suryateja2002@gmail.com>
2021-07-15 13:42:13 -0400

    init

---
+++ src/todosData.js
@@ -0,0 +1,32 @@
+const todosData =[ 
+    {
+        id: 1,
+        text: "take out trash",
+        completed: true
+    },
+
+    {
+        id: 2,
+        text: "Shop",
+        completed: false
+    },
+
+    {
+        id: 3,
+        text: "feed the fish",
+        completed: true
+    },
+
+    {
+        id: 4,
+        text: "Walk the dog",
+        completed: false
+    }
+
+
+    
+
+
+
+]
+export default todosData
